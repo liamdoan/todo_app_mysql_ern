7f34d96bf1df69940dff09c62979042e18064d1d
liamdoan <lamdoan9898@gmail.com>
2024-12-03 11:37:28 +0200

    create initial skeleton for API call function

---
+++ server/controllers/ToDoController.js
@@ -0,0 +1,24 @@
+// get all todos
+module.exports.getTodos = async (req, res) => {
+
+};
+
+// create a new todos
+module.exports.saveToDo = async (req, res) => {
+
+};
+
+// update a todo
+module.exports.updateTodo = (req, res) => {
+
+};
+
+// toggle
+module.exports.toogleCompleted = (req, res) => {
+
+};
+
+// delete a todo
+module.exports.deleteTodo = (req, res) => {
+
+};
