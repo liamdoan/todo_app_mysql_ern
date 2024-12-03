a4c9f5e6d46f2d6aff7cb42004a79b307809d8cf
liamdoan <lamdoan9898@gmail.com>
2024-12-03 11:33:47 +0200

    initial index.js

---
+++ server/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const cors = require('cors');
+const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.listen(PORT, () => {
+    console.log(`Server is up, listening on port ${PORT}`)
+});
