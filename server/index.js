24952b8d5f3dbdd12fb715b935b44967b2b42f87
liamdoan <lamdoan9898@gmail.com>
2024-12-04 04:10:00 +0200

    add routes and use routes

--- server/index.js
+++ server/index.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const cors = require('cors');
 const path = require('path');
+const routes = require('./routes/TodoRoutes');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+app.use('/api', routes);
 
 app.listen(PORT, () => {
     console.log(`Server is up, listening on port ${PORT}`)
