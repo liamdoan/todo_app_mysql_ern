88392ad6e24923cf4c9d518d8c2764179d772005
liamdoan <lamdoan9898@gmail.com>
2024-12-04 04:09:14 +0200

    fix typo

--- server/routes/TodoRoutes.js
+++ server/routes/TodoRoutes.js
@@ -1,11 +1,11 @@
 const express = require('express');
-const { getTodos, saveToDo, updateTodo, deleteTodo, toogleCompleted } = require('../controllers/ToDoController');
+const { getTodos, saveToDo, updateTodo, deleteTodo, toggleCompleted } = require('../controllers/ToDoController');
 const router = express.Router();
 
 router.get('/get', getTodos);
 router.post('/save', saveToDo);
 router.put('/update/:id', updateTodo);
 router.delete('/delete/:id', deleteTodo);
-router.put('/toggle/:id', toogleCompleted);
+router.put('/toggle/:id', toggleCompleted);
 
 module.exports = router;
