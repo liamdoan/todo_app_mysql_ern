5b8d818cbe8a7a3575af97f4a47a92005aedd41a
liamdoan <lamdoan9898@gmail.com>
2024-12-04 08:41:11 +0200

    prettier

--- client/src/App.js
+++ client/src/App.js
@@ -1,79 +1,21 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import { baseUrlDev, baseUrlProd } from './utils/apiUrl';
+import { baseUrlDev } from './utils/apiUrl';
+import { baseUrlProd } from './utils/apiUrl';
 import Spinner from './component/Spinner';
 import './App.css';
 
 const App = () => {
+    const [todos, setTodos] = useState([]);
+    const [todo, setTodo] = useState('');
+    const [desc, setDesc] = useState('');
 
-let time = String(new window.Date())     
-    let date = time.slice(0,25)
+    const [todoEditing, setTodoEditing] = useState(null);
 
-    const [todos, setTodos] = useState([])
-    const [todo, setTodo] = useState("")
-    const [desc, setDesc] = useState("")
-
-    const [todoEditing, setTodoEditing] = useState(null)
-
-    const [editingText, setEditingText] = useState("")
-    const [editingDesc, setEditingDesc] = useState("")
+    const [editingText, setEditingText] = useState('');
+    const [editingDesc, setEditingDesc] = useState('');
 
     const [loading, setLoading] = useState(true);
-
-
-    // SUBMIT
-    function handleSubmit(e) {
-        e.preventDefault()
-
-        const newTodo = {
-            id: new Date().getTime(),
-            timeCreate: "Created at: " + date,
-            timeUpdate: "",
-            text: todo,
-            description: desc,
-            completed: false
-        }
-        setTodos([...todos].concat(newTodo))
-        // reset input after adding newTodo, by setting todo to an empty string
-        setTodo("")
-        setDesc("")
-    }
-
-    // TOGGLE COMPLETE
-    function toggleComplete(id) {
-        const updatedTodos = [...todos].map(todo => {
-            if (todo.id === id) {
-            todo.completed = !todo.completed
-            }
-            return todo
-        })
-        setTodos(updatedTodos)
-    }
-
-    // EDIT TODO
-    function editTodo(id) {
-        const updatedTodos = [...todos].map(todo => {
-            if(todo.id === id ) {
-            todo.text = editingText
-            todo.description = editingDesc
-            todo.timeUpdate = "Update at: " + date
-            }
-            return todo
-        })
-        setTodos(updatedTodos)
-        // reset
-        setTodoEditing(null)
-        setEditingText("")
-        setEditingDesc("")
-    }
-
-    // DELETE
-    function deleteTodo(id) {
-        const updatedTodos = [...todos].filter(todo => todo.id !== id)
-        setTodos(updatedTodos)
-    }
-
-    //--------------------------------------------------
 
     useEffect(() => {
         const fetchdata = async () => {
@@ -96,9 +38,9 @@ let time = String(new window.Date())
         try {
             const res = await axios.post(`${baseUrlDev}/save`, {
                 task_name: todo,
-                task_description: desc
+                task_description: desc,
             });
-            console.log(res.data)
+            console.log(res.data);
             setTodos([...todos, res.data.todo]);
             setTodo('');
             setDesc('');
@@ -112,21 +54,21 @@ let time = String(new window.Date())
         try {
             const res = await axios.put(`${baseUrlDev}/update/${id}`, {
                 task_name: editingText,
-                task_description: editingDesc
-            })
+                task_description: editingDesc,
+            });
 
-            console.log(res.data)
+            console.log(res.data);
 
-            const updatedTodos = todos.map((todo) => 
+            const updatedTodos = todos.map((todo) =>
                 todo.id === id
                     ? {
-                        ...todo,
-                        task_name: res.data.updatedTodo.task_name,
-                        task_description: res.data.updatedTodo.task_description,
-                        updated_at: res.data.updatedTodo.updated_at
-                }
-                : todo
-            )
+                          ...todo,
+                          task_name: res.data.updatedTodo.task_name,
+                          task_description: res.data.updatedTodo.task_description,
+                          updated_at: res.data.updatedTodo.updated_at,
+                      }
+                    : todo
+            );
 
             setTodos(updatedTodos);
             setTodoEditing(null);
@@ -135,7 +77,7 @@ let time = String(new window.Date())
         } catch (error) {
             console.error(error);
         }
-    }
+    };
 
     // Canceling edit/update
     const cancelEditTask = () => {
@@ -148,24 +90,22 @@ let time = String(new window.Date())
     const deleteTask = async (id) => {
         try {
             const res = await axios.delete(`${baseUrlDev}/delete/${id}`);
-            console.log(res.data)
+            console.log(res.data);
             const updatedTodos = [...todos].filter((todo) => todo.id !== id);
             setTodos(updatedTodos);
         } catch (error) {
             console.error(error);
         }
-    }
+    };
 
     // Toggle complete
     const toggleCompleted = async (id) => {
         try {
             const res = await axios.put(`${baseUrlDev}/toggle/${id}`);
-            
-            setTodos(prevTodos => 
-                prevTodos.map(todo => 
-                    todo.id === id 
-                        ? { ...todo, is_completed: res.data.updatedTodo.is_completed }
-                        : todo
+
+            setTodos((prevTodos) =>
+                prevTodos.map((todo) =>
+                    todo.id === id ? { ...todo, is_completed: res.data.updatedTodo.is_completed } : todo
                 )
             );
         } catch (err) {
@@ -179,106 +119,103 @@ let time = String(new window.Date())
             {/* FORM */}
             <form className="todo-form" onSubmit={addTask}>
                 <div className="input-column">
-                <input type="text" 
+                    <input
+                        type="text"
                         placeholder="What to do"
                         onChange={(e) => setTodo(e.target.value)}
                         value={todo}
-                        className="todo-input"/>
-                <input type="text" 
+                        className="todo-input"
+                    />
+                    <input
+                        type="text"
                         placeholder="Description"
                         onChange={(e) => setDesc(e.target.value)}
                         value={desc}
-                        className="desc-input"/>
+                        className="desc-input"
+                    />
                 </div>
-                <button className="submit-button" 
-                        type="submit"
-                        >
-                        Add Tasks
+                <button className="submit-button" type="submit">
+                    Add Tasks
                 </button>
             </form>
             {/* LIST */}
             {loading ? (
                 <Spinner />
             ) : (
-                todos.map(todo => 
-                    <div className="todo-row" 
-                            key={todo.id}>
-                        {todoEditing === todo.id 
-                            ? 
-                            <div className="input-edit-wrap">  
+                todos.map((todo) => (
+                    <div className="todo-row" key={todo.id}>
+                        {todoEditing === todo.id ? (
+                            <div className="input-edit-wrap">
                                 <input
                                     className="input-edit-task"
-                                    type="text" 
-                                    onChange={(e) => setEditingText(e.target.value)} 
+                                    type="text"
+                                    onChange={(e) => setEditingText(e.target.value)}
                                     value={editingText}
                                 />
                                 <input
                                     className="input-edit-desc"
-                                    type="text" 
-                                    onChange={(e) => setEditingDesc(e.target.value)} 
+                                    type="text"
+                                    onChange={(e) => setEditingDesc(e.target.value)}
                                     value={editingDesc}
                                 />
                             </div>
-                            : 
+                        ) : (
                             <div className="input-show">
                                 <p className="input-show-name">{todo.task_name}</p>
                                 <p className="input-show-desc">{todo.task_description}</p>
                                 <p className="input-show-time">
-                                    <span className="span-time">Created at: {new Date(todo.created_at).toLocaleString()}</span>
+                                    <span className="span-time">
+                                        Created at: {new Date(todo.created_at).toLocaleString()}
+                                    </span>
                                 </p>
                                 <p className="input-show-time">
-                                    <span className="span-time">Updated at: {new Date(todo.updated_at).toLocaleString()}</span>
+                                    <span className="span-time">
+                                        Updated at: {new Date(todo.updated_at).toLocaleString()}
+                                    </span>
                                 </p>
                             </div>
-                        }
+                        )}
                         {/* 'todo' is changable */}
                         <div className="buttons">
-                            {todoEditing === todo.id 
-                                ? 
+                            {todoEditing === todo.id ? (
                                 <>
-                                    <button
-                                        className="submit-edit-btn" 
-                                        onClick={() => editTask(todo.id)}
-                                    >
+                                    <button className="submit-edit-btn" onClick={() => editTask(todo.id)}>
                                         Submit Edit
                                     </button>
                                     <button className="delete-btn" onClick={() => cancelEditTask()}>
                                         Cancel
                                     </button>
                                 </>
-                                : 
+                            ) : (
                                 <>
-                                    <input 
-                                        type="checkbox" 
+                                    <input
+                                        type="checkbox"
                                         onChange={() => toggleCompleted(todo.id)}
                                         checked={todo.is_completed}
                                         className="check-complete"
                                     />
                                     <br />
                                     <button
-                                        className="edit-btn" 
+                                        className="edit-btn"
                                         onClick={() => {
-                                            setTodoEditing(todo.id)
-                                            setEditingText(todo.task_name)
-                                            setEditingDesc(todo.task_description)
+                                            setTodoEditing(todo.id);
+                                            setEditingText(todo.task_name);
+                                            setEditingDesc(todo.task_description);
                                         }}
                                     >
                                         Edit Tasks
                                     </button>
-                                    <button 
-                                        className="delete-btn" 
-                                        onClick={() => deleteTask(todo.id)}
-                                    >
+                                    <button className="delete-btn" onClick={() => deleteTask(todo.id)}>
                                         Delete
                                     </button>
                                 </>
-                            }
+                            )}
                         </div>
                     </div>
-                )
+                ))
             )}
         </div>
-    )
-}
+    );
+};
 
 export default App;
