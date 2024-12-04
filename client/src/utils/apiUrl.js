7a61dcc9c060d9ecfbcb3f5a621c4eef286c004f
liamdoan <lamdoan9898@gmail.com>
2024-12-04 05:55:18 +0200

    add base Url utils

---
+++ client/src/utils/apiUrl.js
@@ -0,0 +1,5 @@
+// for local test and development
+export const baseUrlDev = 'http://localhost:5000/api';
+
+// for after deploying to server, use this url instead
+export const baseUrlProd = 'https://todo-app-mern-backendld.vercel.app/api';
